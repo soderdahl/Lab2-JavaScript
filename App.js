76f3be675ad0c807a137caf63698b93b14ff9dbd
soderdahl <43202949+soderdahl@users.noreply.github.com>
2019-04-11 13:23:35 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react'
+import Header from './components/ui/Header/Header'
+import {addBook, fetchBooks} from './utils/api.js'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      books: [],
+      title: '',
+      author: ''
+    }
+
+    this.titleHandler = this.titleHandler.bind(this)
+    this.titleHandler = this.authorHandler.bind(this)
+    this.titleHandler = this.submitHandler.bind(this)
+  }
+
+titleHandler(event) {
+  this.setState({
+    title: event.target.value
+  })
+}
+
+authorHandler(event) {
+  this.setState({
+    author: event.target.value
+  })
+}
+
+submitHandler(event) {
+  event.preventDefault();//förhindra att webläsaren inte ska laddad om sig själv
+  const book ={
+    title: this.state.title,
+    author: this.state.author
+
+  }  
+  addBook(book, (id) => { 
+    this.setState({
+      books: [...this.state.books, {...book, id}]
+    })    
+  })
+}
+
+
+
+
+componentDidUpdate() {
+  fetchBooks(response => {
+    if (response.state == 'success') {
+      this.setState({
+        books: [...response.data]
+      })
+    }
+  })
+}
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="row form-section">
+            <form className="book-form col-6">
+              <legend>Lägg till dina favoritböcker</legend>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  aria-describedby="title"
+                  placeholder="Lägg till titel"
+                  onChange={this.titleHandler}
+                />
+
+                <input
+                  type="text"
+                  className="form-control"
+                  id="author"
+                  rows="3"
+                  data-gramm="true"
+                  data-txt_gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
+                  data-gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
+                  data-gramm_editor="true"
+                  placeholder="Lägg till författare"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg btn-block"
+              >
+                Skicka
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="display-books">
+          <div className="container">
+            <div className="col-12">
+              <ul className="list-group">
+                <li className="list-item list-group-item d-flex align-items-center">
+                  <strong className="title">Titel</strong>
+
+                  <div className="author">Författare</div>
+
+                  <div className="buttons">
+                    <button type="button" className="btn btn-success">
+                      Editera
+                    </button>
+                    <button type="button" className="btn btn-danger">
+                      Ta bort
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <ul>{this.state.books.map(book=>(<li>{book.title}</li>))}</ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
